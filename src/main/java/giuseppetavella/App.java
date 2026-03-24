0c0a0bccebb2dfe6fc9329844762907cd6c985f9
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-03-24 14:57:37 +0100

    DAO Eventi: save done

--- src/main/java/giuseppetavella/App.java
+++ src/main/java/giuseppetavella/App.java
@@ -1,14 +1,42 @@
 package giuseppetavella;
 
+import giuseppetavella.dao.EventiDAO;
+import giuseppetavella.entities.Evento;
+import giuseppetavella.entities.TipoEvento;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.time.LocalDate;
+
 public class App {
 
+    // entity manager factory = 1 connection database
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("gestione-eventi");
     
     public static void main(String[] args) {
         
-        System.out.println("APP WORKS");
+        System.out.println("*** ENTITY MANAGER FACTORY INITIALIZATION: SUCCESS ***");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EventiDAO eventiDAO = new EventiDAO(entityManager);
+        
+        // TODO: in theory you could instantiate an entity with 0 arguments, because 
+        //      the constructor must have 0 parameters for hibernate?
+        Evento evento1 = new Evento(
+                "evento1", 
+                LocalDate.now().plusYears(1),
+                TipoEvento.PRIVATO,
+                3
+        );       
+        
+        eventiDAO.save(evento1);
+        
+        
+        
+        entityManager.close();
+        entityManagerFactory.close();
+        
     }
 }
--- src/main/java/giuseppetavella/dao/EventiDAO.java
+++ src/main/java/giuseppetavella/dao/EventiDAO.java
@@ -4,6 +4,7 @@ import giuseppetavella.entities.Evento;
 import giuseppetavella.exceptions.evento.EventoIDNotFoundException;
 import giuseppetavella.exceptions.evento.SaveEventoException;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 public class EventiDAO {
     
@@ -14,7 +15,19 @@ public class EventiDAO {
     }
     
     public void save(Evento nuovoEvento) throws SaveEventoException {
-          
+        // 1. instantiate new transaction from instance's entity manager
+        EntityTransaction transaction = entityManager.getTransaction();
+        
+        // 2. open new transaction
+        transaction.begin();
+        
+        // 3. save new event in hibernate persistance context
+        // TODO: how to know if there were errors?
+        entityManager.persist(nuovoEvento);
+        
+        // 4. commit or rollback changes from persistance context, into db
+        transaction.commit();
+        
     }
     
     // public Evento getById(long targetId) throws EventoIDNotFoundException {
